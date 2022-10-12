64cc6ff0d4a8a3917bd010efb26f4f561009ccab
Oty Henry <henryotieno66@gmail.com>
2022-10-12 03:42:37 +0300

    Initial commit, started working on Indexing

---
+++ sequences/indexing.py
@@ -0,0 +1,14 @@
+
+#access any item in the sequence using its index. Indexing starts at 0
+
+#string
+name = 'Henry'
+print (name[3]) #the output is r
+
+#list
+animals = ['Goat', 'Donkey', 'Sheep', 'Cow']
+print(animals[1]) #the output is donkey
+
+#tuple
+students = ('Anne', 'Juatine', 'Yvonne', 'Samwel')
+print(students[0]) #the output is Anne
