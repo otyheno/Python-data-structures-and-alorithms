cfca5a74fe3a553067cef13ca20c09ba39aeef7c
Oty Henry <henryotieno66@gmail.com>
2022-10-12 04:26:41 +0300

    added addition and concatenation of sequences

---
+++ sequences/adding_concatenating.py
@@ -0,0 +1,15 @@
+
+#combines 2 sequencesof the same type by using +
+
+
+#string
+y = 'shoe' + 'rack'
+print (y) #output shoerack
+
+# list
+animals = ['Goat', 'Donkey', 'Sheep'] + ['Cow']
+print (animals) #output ['Goat', 'Donkey', 'Sheep', 'Cow']
+
+#turple
+students = ('Anne', 'Juatine', 'Yvonne') + ('Samwel',)
+print(students) #the output is ('Anne', 'Juatine', 'Yvonne', 'Samwel')
