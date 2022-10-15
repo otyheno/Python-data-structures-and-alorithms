e28d6e78f4ae5362d0bc316199015873b8e16742
Oty Henry <henryotieno66@gmail.com>
2022-10-15 08:57:34 +0300

    Worked on unpacking items in a sequence into variables

---
+++ sequences/unpacking.py
@@ -0,0 +1,9 @@
+ #Unpack the n items of a sequence into n variables
+ #the n valiables must match the number of items in the list
+
+ #list
+animals = ['Goat', 'Donkey', 'Sheep', 'Cow']
+
+a, b, c, d = animals
+
+print(a, b, c, d) #the output is Goat Donkey Sheep Cow
