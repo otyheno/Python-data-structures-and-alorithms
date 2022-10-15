f71371edb903e4e885b766fc3c37982e69ed7e48
Oty Henry <henryotieno66@gmail.com>
2022-10-15 03:51:56 +0300

    Worked on multiplying a sequesnce using *

---
+++ sequences/multiplying.py
@@ -0,0 +1,12 @@
+
+#string
+x = 'Bug' *4
+print(x) #Output is BugBugBugBug
+
+#list
+j = [8,7] * 3
+print(j) #output [8, 7, 8, 7, 8, 7]
+
+#tuple
+i = (3,8) *3
+print(i) #output is (3, 8, 3, 8, 3, 8)
