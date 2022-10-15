ae443d00a4ca7d120d183781272881e0e7ca282b
Oty Henry <henryotieno66@gmail.com>
2022-10-15 03:59:03 +0300

    Added check_membership. Which tests whether an item is or is not part of a sequence

---
+++ sequences/check_membership.py
@@ -0,0 +1,13 @@
+#Test whether an item is or is not in a sequence. Returns true or false
+
+#string
+name = 'Henry'
+print ('H' not in name) #the output is false
+
+#list
+animals = ['Goat', 'Donkey', 'Sheep', 'Cow']
+print('Sheep' in animals) #the output is true
+
+#tuple
+students = ('Anne', 'Juatine', 'Yvonne', 'Samwel')
+print("Samwel" not in students) #the output is False
