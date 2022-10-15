b5438878fe79866ec3f06198b4bf375efdec0e5e
Oty Henry <henryotieno66@gmail.com>
2022-10-15 08:27:13 +0300

    Modified number_of_items.py to include count which returns the count of item in a sequence

--- sequences/number_of_items.py
+++ sequences/number_of_items.py
@@ -9,5 +9,18 @@ animals = ['Goat', 'Donkey', 'Sheep', 'Cow']
 print(len(animals)) #the output is 4
 
 #tuple
-students = ('Anne', 'Juatine', 'Yvonne', 'Samwel')
+students = ('Anne', 'Justine', 'Yvonne', 'Samwel')
 print(len(students)) #the output is 4
+
+#count(item) - returns the count n item
+#string
+name = 'Henry'
+print(name.count('e')) #the output is 1
+
+#list
+animals = ['Goat', 'Donkey', 'Sheep', 'Cow']
+print(animals.count("Goat")) #the output is 1
+
+#tuple
+students = ('Anne', 'Justine', 'Anne', 'Yvonne', 'Samwel')
+print(students.count('Anne')) #the output is 2
