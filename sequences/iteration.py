8b7c94ed64bca77e957b5d6cecf70cff7a25a780
Oty Henry <henryotieno66@gmail.com>
2022-10-15 04:11:38 +0300

    Worked on iterating through items in a sequence

---
+++ sequences/iteration.py
@@ -0,0 +1,11 @@
+
+#iterating through the items in a sequence
+#item
+x = [7, 8, 3, 7]
+for item in x:
+    print (item)
+
+#index and item
+y=[7, 8, 3, 7]
+for index, item in enumerate(y):
+    print(index, item)
