542a7ef3d694a79530f8c7856b192be7e4fe73f3
Oty Henry <henryotieno66@gmail.com>
2022-10-15 04:16:18 +0300

    Worked on how to count the number of items (length) in a sequence

---
+++ sequences/number_of_items.py
@@ -0,0 +1,13 @@
+#count the number of items in a sequence
+
+#string
+name = 'Henry'
+print (len(name)) #the output is 5
+
+#list
+animals = ['Goat', 'Donkey', 'Sheep', 'Cow']
+print(len(animals)) #the output is 4
+
+#tuple
+students = ('Anne', 'Juatine', 'Yvonne', 'Samwel')
+print(len(students)) #the output is 4
