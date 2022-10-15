73232805685b5fd9de2fb1ec551978baafa49469
Oty Henry <henryotieno66@gmail.com>
2022-10-15 04:24:22 +0300

    Worked on how to find the minimum or the maximum item in a sequence lexicoraphically

---
+++ sequences/minimum_maximum.py
@@ -0,0 +1,17 @@
+# find the minimum or maximum item in a sequence lexicographically.
+#alpha or numeric types but cannot be mixed
+
+#string
+name = 'Henry'
+print (min(name)) #the output is H
+print (max(name)) #the output is y
+
+#list
+animals = ['Goat', 'Donkey', 'Sheep', 'Cow']
+print(min(animals)) #the output is Cow
+print(max(animals)) #the output is Sheep
+
+#tuple
+students = ('Anne', 'Justine', 'Yvonne', 'Samwel')
+print(min(students)) #the output is Anne
+print(max(students)) #the output is Yvonne
