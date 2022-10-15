fb5b7a04c1bf227d2f13c9eb6c66e711997b92af
Oty Henry <henryotieno66@gmail.com>
2022-10-15 08:44:57 +0300

    Modified indexing.py to add index(item) which returns the index first ocurence of an item

--- sequences/indexing.py
+++ sequences/indexing.py
@@ -10,5 +10,20 @@ animals = ['Goat', 'Donkey', 'Sheep', 'Cow']
 print(animals[1]) #the output is donkey
 
 #tuple
-students = ('Anne', 'Juatine', 'Yvonne', 'Samwel')
+students = ('Anne', 'Justine', 'Yvonne', 'Samwel')
 print(students[0]) #the output is Anne
+
+
+
+#index(item) - returns the index of the first occurence of an item
+#string
+name = 'Henry'
+print (name.index('e')) #the output is r
+
+#list
+animals = ['Goat', 'Donkey', 'Goat', 'Sheep', 'Cow']
+print(animals.index("Goat")) #the output is donkey
+
+#tuple
+students = ('Anne', 'Justine', 'Yvonne', 'Samwel')
+print(students.index('Justine')) #the output is Anne
